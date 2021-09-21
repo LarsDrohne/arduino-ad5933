b6e209778f2fd45116aa39aa60d9953dd88110af
Lars Drohne <45214017+LarsDrohne@users.noreply.github.com>
2021-09-21 16:01:08 +0200

    Update AD5933.cpp

--- AD5933.cpp
+++ AD5933.cpp
@@ -517,7 +517,7 @@ bool AD5933::frequencySweep(int real[], int imag[], int n) {
  * @param n Length of the array (or the number of discrete measurements)
  * @return Success or failure
  */
-bool AD5933::calibrate(double gain[], int phase[], int ref, int n) {
+bool AD5933::calibrate(double gain[], double phase[], int ref, int n) {
     // We need arrays to hold the real and imaginary values temporarily
     int *real = new int[n];
     int *imag = new int[n];
@@ -532,7 +532,7 @@ bool AD5933::calibrate(double gain[], int phase[], int ref, int n) {
     // For each point in the sweep, calculate the gain factor and phase
     for (int i = 0; i < n; i++) {
         gain[i] = (double)(1.0/ref)/sqrt(pow(real[i], 2) + pow(imag[i], 2));
-        // TODO: phase
+        phase[i] = atan2(double(imag[i]), double(real[i])) / 3.14159 * 180.0; // added by Lars
     }
 
     delete [] real;
@@ -552,7 +552,7 @@ bool AD5933::calibrate(double gain[], int phase[], int ref, int n) {
  * @param n Length of the array (or the number of discrete measurements)
  * @return Success or failure
  */
-bool AD5933::calibrate(double gain[], int phase[], int real[], int imag[],
+bool AD5933::calibrate(double gain[], double phase[], int real[], int imag[],
                        int ref, int n) {
     // Perform the frequency sweep
     if (!frequencySweep(real, imag, n)) {
@@ -562,7 +562,7 @@ bool AD5933::calibrate(double gain[], int phase[], int real[], int imag[],
     // For each point in the sweep, calculate the gain factor and phase
     for (int i = 0; i < n; i++) {
         gain[i] = (double)(1.0/ref)/sqrt(pow(real[i], 2) + pow(imag[i], 2));
-        // TODO: phase
+        phase[i] = atan2(double(imag[i]), double(real[i])) / 3.14159 * 180.0; // added by Lars
     }
 
     return true;
